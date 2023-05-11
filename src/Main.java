d6c48bda2755eb2583ccc2e87182cf01854bbe83
Racope <racope9@gmail.com>
2023-05-11 11:23:40 +0200

    V1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Interfaz myFrame = new Interfaz();
-        VentanaBuscar myFrame2 = new VentanaBuscar();
+        VentanaBuscar myFrame2 = new VentanaBuscar(); //perdón
     }
 }
